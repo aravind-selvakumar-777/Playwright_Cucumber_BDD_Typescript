d2baf7ffcc925b45056bd609921689123372e7db
Arima <aravind.selvakumar76@gmail.com>
2025-11-11 21:43:27 +0530

    Adding initial world and hooks with basic options

---
+++ src/support/world.ts
@@ -0,0 +1,18 @@
+import { setWorldConstructor, World } from '@cucumber/cucumber';
+import { Browser, Page, chromium } from 'playwright';
+
+export class CustomWorld extends World {
+  browser!: Browser;
+  page!: Page;
+
+  async openBrowser() {
+    this.browser = await chromium.launch({ headless: false });
+    this.page = await this.browser.newPage();
+  }
+
+  async closeBrowser() {
+    await this.browser.close();
+  }
+}
+
+setWorldConstructor(CustomWorld);
