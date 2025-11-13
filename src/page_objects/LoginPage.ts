ff1b4a62d2a1425abe2a7c707aa338227dd35579
Arima <aravind.selvakumar76@gmail.com>
2025-11-13 21:42:54 +0530

    Added loginpage to hold all methods of login page

---
+++ src/page_objects/LoginPage.ts
@@ -0,0 +1,49 @@
+import { Page, Locator } from 'playwright';
+import { BasePage } from './BasePage';
+
+export class LoginPage extends BasePage {
+  page: Page;
+  usernameTextBox: Locator;
+  passwordTextBox: Locator;
+  LoginButton: Locator;
+  loginErrorMessage: Locator;
+  missingUsernameErrorMessage: Locator;
+  missingPasswordErrorMessage: Locator;
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+    this.usernameTextBox = this.page.getByPlaceholder('Username');
+    this.passwordTextBox = this.page.getByPlaceholder('Password');
+    this.LoginButton = this.page.getByRole('button', { name: 'Login' });
+    this.loginErrorMessage = this.page.locator('div p').first();
+    this.missingUsernameErrorMessage = this.page
+      .locator('span.oxd-input-field-error-message')
+      .first();
+    this.missingPasswordErrorMessage = this.page
+      .locator('span.oxd-input-field-error-message')
+      .nth(1);
+  }
+  async goToOrange(url: string) {  //XXXXX -- NEED TO PUSH THIS TO BASEPAGE -- XXXXX
+    await this.page.goto(url, { waitUntil: 'load' });
+  }
+  async fillUsernameAndPassword(username: string, password: string) {
+    await this.usernameTextBox.fill(username);
+    await this.passwordTextBox.fill(password);
+  }
+  async clickLoginButton() {
+    await this.click(this.LoginButton);
+  }
+  async getCurrentURL(): Promise<string> {
+    return this.page.url();
+  }
+
+  async getInvalidLoginErrorText(): Promise<string> {
+    return this.getText(this.loginErrorMessage);
+  }
+  async getMissingUsernameErrorText(): Promise<string> {
+    return this.getText(this.missingUsernameErrorMessage);
+  }
+  async getMissingPasswordErrorText(): Promise<string> {
+    return this.getText(this.missingPasswordErrorMessage);
+  }
+}
