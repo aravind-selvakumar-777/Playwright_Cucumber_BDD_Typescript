e70c0280b6ecdba197592a448b380caf01475117
Arima <aravind.selvakumar76@gmail.com>
2025-11-18 17:20:07 +0530

    Forgot to commit world file which holds PO manager setup

--- src/support/world.ts
+++ src/support/world.ts
@@ -1,9 +1,11 @@
 import { setWorldConstructor, World } from '@cucumber/cucumber';
 import { Browser, Page, chromium } from 'playwright';
+import { PageObjectManager } from '../page_objects/PageObjectManager';
 
 export class CustomWorld extends World {
   browser!: Browser;
   page!: Page;
+  pageObjectManager!: PageObjectManager;
 
   async openBrowser() {
     this.browser = await chromium.launch();
