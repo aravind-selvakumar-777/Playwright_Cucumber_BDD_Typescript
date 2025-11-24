c070afc7f5a9ea858d0bdde9c4b877c2edf9d05d
Arima <aravind.selvakumar76@gmail.com>
2025-11-24 15:46:49 +0530

    SideMenu component Implemented

---
+++ src/page_objects/components/SideMenu.ts
@@ -0,0 +1,30 @@
+import { Locator, Page } from 'playwright';
+import { BasePage } from '../BasePage';
+
+export class SideMenu extends BasePage {
+  readonly options = {
+    admin: 'Admin',
+    pim: 'PIM',
+    leave: 'Leave',
+    time: 'Time',
+    recruitment: 'Recruitment',
+    info: 'My info',
+    performance: 'Perforance',
+    dashboard: 'Dashboard',
+    directory: 'Directory',
+    claim: 'Claim',
+    buzz: 'Buzz',
+  } as const;
+  page: Page;
+  rootElement: Locator;
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+    this.rootElement = this.page.locator('li.oxd-main-menu-item-wrapper');
+  }
+
+  async clickSideMenuItem(name: keyof typeof this.options) {
+    await this.click(this.rootElement.locator(' span', { hasText: this.options[name] }));
+    await this.page.waitForLoadState('networkidle');
+  }
+}
