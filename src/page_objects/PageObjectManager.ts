1697c2acfae78b8cb5ddab0b8e2e3b5e6abc90bd
Arima <aravind.selvakumar76@gmail.com>
2025-12-01 23:04:52 +0530

    Pim page and admin page added to PO manager

--- src/page_objects/PageObjectManager.ts
+++ src/page_objects/PageObjectManager.ts
@@ -2,17 +2,23 @@ import { Page } from 'playwright';
 import { LoginPage } from './LoginPage';
 import { RecruitmentPage } from './RecruitmentPage';
 import { DirectoryPage } from './DirectoryPage';
+import { PIMPage } from './PIMPage';
+import { AdminPage } from './AdminPage';
 
 export class PageObjectManager {
   page: Page;
   loginPage: LoginPage;
   recruitmentPage: RecruitmentPage;
   directoryPage: DirectoryPage;
+  pimPage: PIMPage;
+  adminPage: AdminPage;
   constructor(page: Page) {
     this.page = page;
     this.loginPage = new LoginPage(this.page);
     this.recruitmentPage = new RecruitmentPage(this.page);
     this.directoryPage = new DirectoryPage(this.page);
+    this.pimPage = new PIMPage(this.page);
+    this.adminPage = new AdminPage(this.page);
   }
 
   getLoginPage(): LoginPage {
@@ -23,5 +29,11 @@ export class PageObjectManager {
   }
   getDirectoryPage(): DirectoryPage {
     return this.directoryPage;
+  }
+  getPIMPage(): PIMPage {
+    return this.pimPage;
+  }
+  getAdminPage(): AdminPage {
+    return this.adminPage;
   }
 }
